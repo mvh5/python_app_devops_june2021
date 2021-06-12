6d12d0aec7728c117a527188873d3f9e67fe30b5
Account2 <account2@Mauricios-MacBook-Air.local>
2021-06-12 13:03:39 -0500

    modified run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #echo "docker run -itd --name test_api01 --rm mauriciovh05/api_cdk_may:$1"
-docker run -itd --name test_api01 --rm mauriciovh05/api_cdk_may:$1 
+docker run -itd --name test_api01 -p 3000:3000  --rm mauriciovh05/api_cdk_may:$1 
